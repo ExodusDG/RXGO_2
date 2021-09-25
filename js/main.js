8f448e2530b12f605015f88962d124c43a0901cf
Nikolay <68652897+SanistRay@users.noreply.github.com>
2021-09-25 18:46:27 +0300

    1

---
+++ js/main.js
@@ -0,0 +1,43 @@
+/* DISCOUNT ADRESS  */
+
+$('.discount__adress').hover(
+    function() {
+        $(this).addClass('discount__text_hover')
+        $(this).find('span').css('display', 'block')
+    },
+    function() {
+        $('.discount__adress').find('span').css('display', 'none')
+        $('.discount__adress').removeClass('discount__text_hover')
+    })
+
+$('.discount__item_show').click(function() {
+    $(this).closest('.discount__item_wrapper').find('.discount__adress').toggleClass('discount__adress_open')
+})
+
+/* FAQ ITEM */
+
+$('.faq__item').click(function() {
+
+    if ($(this).find('.faq__item_answer').hasClass('faq__item_answer_open')) {
+        $(this).find('.faq__item_status').text('+')
+    } else {
+        $(this).find('.faq__item_status').text('-')
+    }
+
+    $(this).find('.faq__item_answer').toggleClass('faq__item_answer_open');
+    $(this).find('.faq__answer__text').toggleClass('faq__answer__text_visible')
+
+    $(this).find('.faq__item_number').toggleClass('faq__item_number_open')
+})
+
+var bodyWidth = $('body').width()
+
+if (bodyWidth < 491) {
+    $('.info__full_logo').attr('src', 'image/logotypes__mb.png')
+
+}
+
+if (bodyWidth < 1024) {
+
+    $('.coupon__price > img').attr('src', 'image/Component 16.png')
+}
